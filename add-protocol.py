a46de7a1e765d017c5ed14da2487f46b474541a4
Midas van Veen <midasvanveen.email@gmail.com>
2023-08-26 20:05:32 +0200

    fixed script

--- add-protocol.py
+++ add-protocol.py
@@ -2,9 +2,11 @@
 import sys
 import os
 
-if not sys.argv[1] or not sys.argv[2]:
-	print(usage)
-	os.exit(-1)
+try:
+	sys.argv[1]
+except IndexError:
+	print("Usage: add-protocol.py <file> <protocol>")
+	sys.exit(1)
 
 with open(sys.argv[1]) as f:
 	for line in f.readlines():
